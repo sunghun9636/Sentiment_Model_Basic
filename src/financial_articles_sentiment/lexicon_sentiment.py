0611e902f709f673725215c588235787fa239c0f
Sunghun Jung <sunghunjung96@gmail.com>
2018-06-20 12:14:01 +0100

    adding a function to flip a sentiment value when prefixed by negation word

--- src/financial_articles_sentiment/lexicon_sentiment.py
+++ src/financial_articles_sentiment/lexicon_sentiment.py
@@ -68,7 +68,7 @@ class Lexicon_sentiment(object):
     # Return True if index != 0 and tokens[i-1] is in self.negations else False
     def is_prefixed_by_negation(self, token_index, tokens):
         prev_index = token_index - 1
-        if tokens[prev_index] in self.negation_skip:
+        if tokens[prev_index] in self.negation_skip or tokens[prev_index] in self.intensifier:
             prev_index -= 1 # if the previous token is skip word, shift the index by -1
 
         is_prefixed_by_negation = False
@@ -77,6 +77,12 @@ class Lexicon_sentiment(object):
             is_prefixed_by_negation = True
 
         return is_prefixed_by_negation
+
+    # function to adjust the sentiment value with negation word
+    def negate_sentiment(self, sentiment):
+        flipped_sentiment = sentiment * -1
+
+        return 0.5 * flipped_sentiment
 
     # function to return the impact of intensifier prior to the current word
     def intensified_by(self, token_index, tokens):
@@ -93,7 +99,7 @@ class Lexicon_sentiment(object):
         # clean up the text first and change it into uppercase letters
         text_clean = re.sub(r'[^\w ]', ' ', text.upper())
         useful_tokens = text_clean.split()
-        useful_tokens = [word for word in useful_tokens if word not in stopwords.words('english')]
+        # useful_tokens = [word for word in useful_tokens if word not in stopwords.words('english')]
 
         scores = defaultdict(float) # sentiment scores to be stored
         # words = defaultdict(list) # positive and negative words detected to be stored
@@ -104,11 +110,17 @@ class Lexicon_sentiment(object):
             intensified_by = self.intensified_by(i, useful_tokens)
 
             # if the word is in the word list and not prefixed by negations word:
-            if token in self.word_list and not is_prefixed_by_negation:
-                score = self.word_list[token] * intensified_by
-                score_type = 'negative' if score < 0 else 'positive'
-                scores[score_type] += score
-                # words[score_type].append(token)
+            if token in self.word_list:
+                if not is_prefixed_by_negation:
+                    score = self.word_list[token] * intensified_by
+                    score_type = 'negative' if score < 0 else 'positive'
+                    scores[score_type] += score
+                    # words[score_type].append(token)
+                else:
+                    score = self.word_list[token] * intensified_by
+                    score = self.negate_sentiment(score)
+                    score_type = 'negative' if score < 0 else 'positive'
+                    scores[score_type] += score
 
         if len(useful_tokens) > 0:
             # calculate comparative score / result to output
@@ -120,10 +132,10 @@ class Lexicon_sentiment(object):
 def main():
     sentiment = Lexicon_sentiment()
 
-    article = "company is doing slightly abnormal compared to last year"
+    article = "company is doing not really abnormal compared to last year"
 
     result = sentiment.assign_sentiment(article)
     print(result)
-
+    
 if __name__ == '__main__':
     main()
