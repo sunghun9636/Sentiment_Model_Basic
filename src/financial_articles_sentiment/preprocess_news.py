54a83aa71780048f0803212c5c1cf1b8a263a8f8
Sunghun Jung <sunghunjung96@gmail.com>
2018-06-26 16:10:05 +0100

    finished basic preprocessing

--- src/financial_articles_sentiment/preprocess_news.py
+++ src/financial_articles_sentiment/preprocess_news.py
@@ -11,17 +11,27 @@ from nltk.stem import LancasterStemmer, WordNetLemmatizer
 # to be used to filter paragraphs stating factual numbers only
 def is_numerical(text):
     count = 0 # counter for numerical figures / expressions
+    maximum_numericals = 5 # TODO: change the 'n' to appropriate number
     words = nltk.word_tokenize(text)
     for word in words:
         if word.isdigit():
             count += 1
 
-    return count > 5 # TODO: change the 'n' to appropriate number
+    return count > maximum_numericals
+
+# return if the paragraph contain the company name or not
+def has_company_name(text, name):
+    words = nltk.word_tokenize(text)
+
+    if name in words:
+        return True
+    else:
+        return False
 
 ## Noise Removal
 
+# e.g. "I'm doing this" -> "I am doing this"
 def replace_contractions(text):
-    # e.g. "I'm doing this" -> "I am doing this"
     return contractions.fix(text)
 
 ## Normalization
@@ -50,9 +60,21 @@ def remove_punctuation(words):
             new_words.append(new_word)
     return new_words
 
+def remove_stopwords(words):
+    new_words = [word for word in words if word not in stopwords.words('english')]
+    return new_words
+
+def lemmatize_verbs(words):
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos = 'v')
+        lemmas.append(lemma)
+    return lemmas
 
 ## Main function to preprocess the financial news articles
-def preprocess(text):
+def preprocess(text, company_name):
     # 1. extract meaningful paragraphs
     paragraphs = text.split('\n')
     # a. remove paragraphs with many numerical figures
@@ -61,9 +83,14 @@ def preprocess(text):
         if not is_numerical(paragraph):
             non_numerical_paragraphs.append(paragraph)
     # b. remove paragraph that doesn't mention the company at all
-    # c. TODO: further remove unnecessary paragraphs
-    clean_text = []
+    useful_paragraphs = []
     for paragraph in non_numerical_paragraphs:
+        if has_company_name(paragraph, company_name):
+            useful_paragraphs.append(paragraph)
+    # c. TODO: further remove unnecessary paragraphs
+
+    clean_text = []
+    for paragraph in useful_paragraphs:
         clean_text.append(' ')
         clean_text.append(paragraph)
     clean_text = ''.join(clean_text)
@@ -75,10 +102,17 @@ def preprocess(text):
     words = nltk.word_tokenize(clean_text)
 
     # 4. Normalization
+    words = remove_non_ascii(words)
+    words = to_uppercase(words)
+    words = remove_punctuation(words)
+    words = remove_stopwords(words)
+    words = lemmatize_verbs(words)
+
+    return words
 
 if __name__ == '__main__':
-    text = "Company A has increased its stock price by 5%. I'm testing.\n I am testing"
-    paragraphs = text.split('\n')
-    words = nltk.word_tokenize(paragraphs[1])
+    text = "Facebook is doing really poorly this year.\n Also, its stock price has dropped by 5%, ..."
+    company_name = "Facebook"
+    words = preprocess(text, company_name)
     for word in words:
         print(word)
